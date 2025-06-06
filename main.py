1c9091465fcdd5e355cfeb1687dd5a017c326780
Jasraj-Jassar <jasrajjassar775@gmail.com>
2025-06-05 22:59:19 -0600

    revert: remove animation changes

--- main.py
+++ main.py
@@ -2,24 +2,18 @@ import tkinter as tk
 from grid import make_grid
 from remove_line import clear_grid_line
 from standardize import standardize_start_end
+from random_grid import random_grid_genrator
 
 # Initialize main window
 window = tk.Tk()
 
 # Define grid dimensions
 box_size = 160  # Size of each grid cell
-margin = 160    # Margin around the grid
 
-maze = [
-    [1,1,1,1,1],
-    [1,0,0,0,1],
-    [1,0,1,0,1],
-    [1,0,1,0,1],
-    [1,1,1,1,1]
-]
+maze = random_grid_genrator(8, 8)
 
 # Create the grid and standardize start/end positions
 board = make_grid(maze, box_size, window)
-standard_start_end = standardize_start_end(box_size, margin, board)
+standard_start_end = standardize_start_end(box_size, board)
 # Start the main event loop
 window.mainloop() 
